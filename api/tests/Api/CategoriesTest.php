eea3e133ebc98728abba2b6035e29ddd3990bc82
Benjamin Gleitz <gleitz.benjamin@gmail.com>
2025-09-27 20:19:16 +0200

    create test for categories

---
+++ api/tests/Api/CategoriesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+class CategoriesTest extends ApiTestCase
+{
+    public function testCreateCategory(): void
+    {
+        static::createClient()->request('POST', '/categories', [
+            'json' => [
+                'title' => 'Développement',
+                'description' => 'Compétences liées au développement logiciel',
+            ],
+            'headers' => [
+                'Content-Type' => 'application/ld+json',
+            ],
+        ]);
+
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertJsonContains([
+            '@context' => '/contexts/Category',
+            '@type' => 'Category',
+            'title' => 'Développement',
+            'description' => 'Compétences liées au développement logiciel',
+        ]);
+    }
+}
